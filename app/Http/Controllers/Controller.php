944f8869edcce350d422ea8b532d6e7346ab01a4
MD031217 <m_dolmatova03@bk.ru>
2026-03-05 16:35:03 +0500

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class Controller extends BaseController
+class Controller
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function serverInfo()
+    {
+        return view('info', [
+            'title' => 'Информация о сервере',
+            'php_version' => phpversion(),
+            'php_extensions' => implode(', ', get_loaded_extensions()),
+            'server_software' => $_SERVER['SERVER_SOFTWARE'] ?? 'Unknown',
+        ]);
+    }
+
+    public function clientInfo()
+    {
+        return view('info', [
+            'title' => 'Информация о клиенте',
+            'ip_address' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+        ]);
+    }
+    public function databaseInfo()
+    {
+        try {
+            $dbInfo = DB::select('SELECT VERSION() as version');
+            $version = $dbInfo[0]->version;
+            $driver = config('database.default');
+            $database = config('database.connections.' . $driver . '.database');
+        } catch (\Exception $e) {
+            $version = 'Ошибка подключения';
+            $driver = config('database.default');
+            $database = config('database.connections.' . $driver . '.database');
+        }
+
+        return view('info', [
+            'title' => 'Информация о базе данных',
+            'driver' => $driver,
+            'db_version' => $version,
+            'db_name' => $database,
+        ]);
+    }
 }
